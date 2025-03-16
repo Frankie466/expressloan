4eb734e86ec14efb758e397c111300e495ac7bdc
Frankline Kibet <144028224+Frankie466@users.noreply.github.com>
2025-03-16 09:01:16 +0300

    Update settings.py

--- loan/settings.py
+++ loan/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j&32z$hr_2gq_5la*8g@gy^_wkane5eu+2a+92ybxt)67__!z8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','.now.sh']
 
